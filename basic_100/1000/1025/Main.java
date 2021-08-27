642bbb8af6f33acbb1197d01c262dbd9f9829943
TakJIAe <hiworldd@naver.com>
2021-08-27 14:45:20 +0900

    정수 1개 입력받아 나누어 출력하기

---
+++ basic_100/1000/1025/Main.java
@@ -0,0 +1,22 @@
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+
+// public class Main {
+//     public static void main(String[] args) throws IOException {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         String num = br.readLine();
+//         String arr[]=num.split("");
+        
+//         for(int i=0; i<arr.length; i++){
+//             System.out.printf("[");
+//             System.out.printf(arr[i]);
+            
+//             for(int j=arr.length-1; j>i; j--){
+//                 System.out.printf("0");
+//             }
+//             System.out.printf("]");
+//             System.out.printf("\n");
+//         }
+//     }
+// }
