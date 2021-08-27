d02d2a826ae0831092e8bb2caa60096a9c56f4f1
TakJIAe <hiworldd@naver.com>
2021-08-27 10:04:07 +0900

    출력하기08

---
+++ basic_100/1000/1008/Main.java
@@ -0,0 +1,7 @@
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.printf("\u250C\u252C\u2510\n");
+//         System.out.printf("\u251C\u253C\u2524\n");
+//         System.out.printf("\u2514\u2534\u2518");
+//     }
+// }
