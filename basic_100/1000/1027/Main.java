7537b823090692fcfa43c94207e3b175193d4d2c
TakJIAe <hiworldd@naver.com>
2021-08-27 14:59:24 +0900

    년월일 입력 받아 형식 바꿔 출력하기

---
+++ basic_100/1000/1027/Main.java
@@ -0,0 +1,13 @@
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+
+// public class Main {
+//     public static void main(String[] args) throws IOException {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         String input = br.readLine();
+//         String arr[] = input.split("\\.");
+
+//         System.out.printf("%s-%s-%s", arr[2], arr[1], arr[0]);
+//     }
+// }
