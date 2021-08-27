1d40a4b7b66f8964eb16cc39e7e6a8ac01729c67
TakJIAe <hiworldd@naver.com>
2021-08-27 13:27:28 +0900

    주민번호 입력받아 형태 바꿔 출력하기

---
+++ basic_100/1000/1020/Main.java
@@ -0,0 +1,17 @@
+// import java.util.Scanner;
+// import java.util.StringTokenizer;
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+        
+//         String PRN = sc.nextLine();
+//         StringTokenizer stn = new StringTokenizer(PRN,"-");
+        
+//         String num1 = stn.nextToken();
+//         String num2 = stn.nextToken();
+        
+//         sc.close();
+        
+//         System.out.printf("%s%s", num1, num2);
+//     }
+// }
