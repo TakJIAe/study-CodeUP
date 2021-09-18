3e432c821c2ba5446125f4b401fa4f073b2a46eb
TakJIAe <hiworldd@naver.com>
2021-09-18 13:05:45 +0900

    정수 3개 입력받아 합과 평균 출력하기

---
+++ basic_100/1000/1046/Main.java
@@ -0,0 +1,14 @@
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int c = sc.nextInt();
+
+//         float n = (float)(a+b+c);
+//         System.out.println(a+b+c);
+//         System.out.printf("%.1f",n/3);
+//     }
+// }
